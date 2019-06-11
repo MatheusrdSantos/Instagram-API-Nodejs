ad69bfb0747b928745fbcbd1f150602ce5215dd3
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-11 20:17:03 -0300

    Route to add likes implemented

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload'); 
 const routes = express.Router();
 const PostController = require('./controllers/PostController');
-
+const LikeController = require('./controllers/LikeController');
 const upload = multer(uploadConfig);
 
 
 routes.post('/posts', upload.single('image'),PostController.store);
 routes.get('/posts', PostController.index);
+
+routes.post('/posts/:id/like', LikeController.store);
 
 
 
