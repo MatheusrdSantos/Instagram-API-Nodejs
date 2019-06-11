6c8831ffc86b837325d560bb8bd08f6ec4968a34
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-11 20:28:32 -0300

    Image resizing implemented

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     async index(req, res){
@@ -9,6 +12,7 @@ module.exports = {
         const {author, place, description, hashtags} = req.body;
         const { filename: image } = req.file;
         //console.log(req.file)
+        await sharp(req.file.path).resize(500).jpeg({quality:70}).toFile(path.resolve(req.file.destination, 'resized', image));
         const post = await Post.create({
             author,
             place,
@@ -16,6 +20,7 @@ module.exports = {
             hashtags,
             image,
         });
+        fs.unlinkSync(req.file.path);
         return res.json(post);
     }
 };
