8018d41514ce574676aeed287e0ba0a35982d832
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-11 19:57:46 -0300

    Route to create posts implemented

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const multer = require('multer');
+const uploadConfig = require('./config/upload'); 
 const routes = express.Router();
+const PostController = require('./controllers/PostController');
 
-routes.get('/', (req, res) => {
-    return res.send(`Olá, ${req.query.name}`);
-});
+const upload = multer(uploadConfig);
 
-module.exports(routes);
+
+routes.post('/posts', upload.single('image'),PostController.store);
+
+
+
+module.exports = routes;
