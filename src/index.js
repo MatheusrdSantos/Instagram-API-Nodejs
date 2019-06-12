bfd883037c446fd18a5c073ed1295a12e88d6303
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-12 20:22:41 -0300

    cors declaration fixed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 
 const app = express();
+app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -14,11 +15,11 @@ mongoose.connect('mongodb+srv://root:0A072mNLwZMoqNlj@cluster0-pmatr.mongodb.net
 
 app.use(require('./routes'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
-app.use(cors());
 app.use((req, res, next)=>{
     req.io = io;
     
     next();
-})
+});
+
 server.listen(3333);
 
